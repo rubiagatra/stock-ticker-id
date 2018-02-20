af992e76a83163f6e400f353e2074b4467a850bb
Doni Rubiagatra <rubiagatra@gmail.com>
2018-02-20 11:00:50 +0700

    update stock predict python

--- stock-prediction/stockpredict.py
+++ stock-prediction/stockpredict.py
@@ -9,13 +9,18 @@ class StockPredict:
     def __init__(self):
         self.close = None
         self.forecast = None
-
+        self.prediction = None
+        self.end = None
+        self.pred_df = None
+        self.name = None
+        
     def get_data(self, name, start_date, end_date):
+        self.name = name
         start_split = start_date.split('-')
         end_split = end_date.split('-') 
         start = datetime.datetime(int(start_split[0]), int(start_split[1]), int(start_split[2]))
-        end = datetime.datetime(int(end_split[0]), int(end_split[1]), int(end_split[2]))
-        df = web.DataReader(name=name, data_source='google', start=start, end=end)
+        self.end = datetime.datetime(int(end_split[0]), int(end_split[1]), int(end_split[2]))
+        df = web.DataReader(name=name, data_source='google', start=start, end=self.end)
         self.close = df['Close']
 
     def predict(self, days):
@@ -27,6 +32,27 @@ class StockPredict:
         forecast = model.predict(future)
         self.forecast = forecast.set_index('ds')['yhat']
         self.forecast = np.exp(self.forecast)
+        self.forecast.name = self.name
+        self.pred_df = self.forecast[self.end:]
 
-    
+
+class MultiPredict:
+    def __init__(self, list_name):
+        self.list_name = list_name
+        self.prediction = [] 
+
+    def predict(self, start, end, days):
+        for x in self.list_name:
+            forecast = StockPredict()
+            forecast.get_data(x, start, end)
+            forecast.predict(days)
+            self.prediction.append(forecast.pred_df)
+        self.prediction = pd.DataFrame(self.prediction).T
+
+    def to_csv(self, name):
+        self.prediction.to_csv(name)
+
+
+        
+
 
